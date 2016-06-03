1cc8374e549fdf42d9c1ec8ba89d20ca4edb50a6
Sebastián Mancilla <smancill@jlab.org>
2016-06-02 21:04:15 -0400

    Remove JToolBox dependency
    The JPropertyList class is not used anymore, and there is no need to
    depend on an entire jar to get only the EviO mime-type string. Just put
    the mime-type name directly in the source.

--- src/main/java/org/jlab/clas/std/services/util/Clas12Types.java
+++ src/main/java/org/jlab/clas/std/services/util/Clas12Types.java
@@ -1,12 +1,6 @@
 package org.jlab.clas.std.services.util;
 
-import java.nio.ByteBuffer;
-
-import org.jlab.clara.base.error.ClaraException;
-import org.jlab.clara.engine.ClaraSerializer;
 import org.jlab.clara.engine.EngineDataType;
-import org.jlab.clas12.tools.MimeType;
-import org.jlab.clas12.tools.property.JPropertyList;
 
 // TODO: put this in a common CLAS package
 public final class Clas12Types {
@@ -14,22 +8,5 @@ public final class Clas12Types {
     private Clas12Types() { }
 
     public static final EngineDataType EVIO =
-            new EngineDataType(MimeType.EVIO.type(), EngineDataType.BYTES.serializer());
-
-    public static final EngineDataType PROPERTY_LIST =
-            new EngineDataType(MimeType.PROPERTY_LIST.type(),
-                new ClaraSerializer() {
-
-                    @Override
-                    public ByteBuffer write(Object data) throws ClaraException {
-                        JPropertyList pl = (JPropertyList) data;
-                        return ByteBuffer.wrap(pl.getStringRepresentation(true).getBytes());
-                    }
-
-
-                    @Override
-                    public Object read(ByteBuffer buffer) throws ClaraException {
-                        return new JPropertyList(new String(buffer.array()));
-                    }
-                });
+            new EngineDataType("binary/data-evio", EngineDataType.BYTES.serializer());
 }
